b76b1bd52bc335f0c9961d01fa22d054e7e9a1aa
ChanukOh <cutycater@gmail.com>
2024-05-29 10:24:57 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,12 @@
+def solution(letter):
+    morse_to_alpha = {
+        '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e', '..-.': 'f',
+        '--.': 'g', '....': 'h', '..': 'i', '.---': 'j', '-.-': 'k', '.-..': 'l',
+        '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q', '.-.': 'r',
+        '...': 's', '-': 't', '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x',
+        '-.--': 'y', '--..': 'z'
+    }
+    
+    morse_code = letter.split(' ')
+    answer = ''.join([morse_to_alpha[code] for code in morse_code if code in morse_to_alpha])
+    return answer
