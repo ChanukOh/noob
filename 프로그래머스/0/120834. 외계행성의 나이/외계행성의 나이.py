a746db4f910567cbdcaa2b34f02962f8f4619c40
ChanukOh <cutycater@gmail.com>
2024-05-30 17:42:55 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,24 +1,3 @@
 def solution(age):
-    a=[]
-    for i in str(age):
-        if i=="1":
-            a.append('b')
-        if i=="2":
-            a.append('c')
-        if i=="3":
-            a.append('d')
-        if i=="4":
-            a.append('e')
-        if i=="5":
-            a.append('f')
-        if i=="6":
-            a.append('g')
-        if i=="7":
-            a.append('h')
-        if i=="8":
-            a.append('i')
-        if i=="9":
-            a.append('j')
-        if i=="0":
-            a.append('a')
-    return ''.join([str(x) for x in a])
+    table=str.maketrans('0123456789','abcdefghij')
+    return str(age).translate(table)
