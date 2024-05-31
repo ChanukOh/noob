9a96dd01329ed92e774ea129ceb906ab5e3dbc01
ChanukOh <cutycater@gmail.com>
2024-05-31 14:04:18 +0900

    [level 1] Title: 모의고사, Time: 3.96 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -2,7 +2,7 @@ def solution(answers):
     answer=[]
     a=[1,2,3,4,5]*((len(answers)//5)+2)
     b=[2,1,2,3,2,4,2,5]*((len(answers)//8)+2)
-    c=[3,3,1,1,2,2,4,4,5,5]*((len(answers)//8)+2)
+    c=[3,3,1,1,2,2,4,4,5,5]*((len(answers)//10)+2)
     acount=0
     bcount=0
     ccount=0
