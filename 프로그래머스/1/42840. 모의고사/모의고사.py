abdc5565a306b88cd1ebb8b54eb643173ac4bb88
ChanukOh <cutycater@gmail.com>
2024-05-31 09:24:48 +0900

    [level 1] Title: 모의고사, Time: 2.08 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,26 @@
+def solution(answers):
+    answer=[]
+    a=[1,2,3,4,5]*((len(answers)//5)+2)
+    b=[2,1,2,3,2,4,2,5]*((len(answers)//8)+2)
+    c=[3,3,1,1,2,2,4,4,5,5]*((len(answers)//8)+2)
+    acount=0
+    bcount=0
+    ccount=0
+    for i in range(len(answers)):
+        if answers[i]==a[i]:
+            acount+=1
+        if answers[i]==b[i]:
+            bcount+=1
+        if answers[i]==c[i]:
+            ccount+=1
+
+    max_count = max(acount, bcount, ccount)
+
+    if acount == max_count:
+        answer.append(1)
+    if bcount == max_count:
+        answer.append(2)
+    if ccount == max_count:
+        answer.append(3)
+
+    return answer
