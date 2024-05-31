3ff166fb912a08a260e86d639977502337259ba3
ChanukOh <cutycater@gmail.com>
2024-05-31 10:44:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.32 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,9 @@
+def solution(participant, completion):
+    participant_count={}
+    for p in participant:
+        participant_count[p]=participant_count.get(p,0)+1
+    for c in completion:
+        participant_count[c]-=1
+    for p,count in participant_count.items():
+        if count>0:
+            return p
