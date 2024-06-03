e55ba8faaeba7370e924f13119bfa46262b5adbf
ChanukOh <cutycater@gmail.com>
2024-06-03 11:25:40 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -4,4 +4,4 @@ def solution(my_string, num1, num2):
     my_string=list(my_string)
     my_string[num1]=b
     my_string[num2]=a
-    return ''.join(i for i in my_string)
+    return ''.join(my_string)
