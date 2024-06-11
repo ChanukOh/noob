8289d91d886e5ba1e296581c659de8a923d7b220
ChanukOh <cutycater@gmail.com>
2024-06-11 10:34:44 +0900

    [level 2] Title: 타겟 넘버, Time: 1.40 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,25 @@
+graph={}
+for i in range(1,51):
+    graph[i]=[i,-i]
+
+def solution(numbers, target):
+    def finder(numbers, found=None):
+        if found is None:
+            found = []
+
+        if not numbers:
+            return found
+
+        current_digit = numbers[0]
+        if current_digit in graph:
+            new_found = []
+            for letter in graph[current_digit]:
+                if not found:
+                    new_found.append(letter)
+                else:
+                    for item in found:
+                        new_found.append(item + letter)
+            return finder(numbers[1:], new_found)
+        else:
+            return found
+    return finder(numbers).count(target)
