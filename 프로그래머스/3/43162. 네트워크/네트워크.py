0a76b874e821e6bfc78bc07bfbd4ce295f07c5fb
ChanukOh <cutycater@gmail.com>
2024-06-13 15:18:58 +0900

    [level 3] Title: 네트워크, Time: 45.93 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,20 +1,10 @@
-def solution(n, computers):
-    def dfs(computers, visited, node):
-        stack = [node]
-        while stack:
-            current = stack.pop()
-            if not visited[current]:
-                visited[current] = True
-                for idx, connected in enumerate(computers[current]):
-                    if connected and not visited[idx]:
-                        stack.append(idx)
+def solution(n,computers):
+    answer=list(range(n))
 
-    num_networks = 0
-    visited = [False] * n
-
-    for node in range(n):
-        if not visited[node]:
-            dfs(computers, visited, node)
-            num_networks += 1
-
-    return num_networks
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if computers[i][k]==1 and computers[k][j]==1:
+                    computers[i][j]=1
+                    answer[j]=answer[i]
+    return len(set(answer))
