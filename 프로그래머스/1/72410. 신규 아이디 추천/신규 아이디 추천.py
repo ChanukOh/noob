a6a3beed57503e053b25c357c219769a5b9c89f3
ChanukOh <cutycater@gmail.com>
2024-06-27 17:46:02 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.23 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,15 @@
+import re
+
+def solution(new_id):
+    new_id = new_id.lower()
+    new_id = re.sub(r'[^a-z0-9\-_.]', '', new_id)
+    new_id = re.sub(r'\.{2,}', '.', new_id)
+    new_id = new_id.strip('.')
+    if new_id == '':
+        new_id = 'a'
+    if len(new_id) >= 16:
+        new_id = new_id[:15].rstrip('.')
+    while len(new_id) < 3:
+        new_id += new_id[-1]
+    
+    return new_id
