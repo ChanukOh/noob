a3289fe97e19cf893816ca2aac3a0a1782dc0569
ChanukOh <cutycater@gmail.com>
2024-07-05 11:37:32 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 28.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def solution(arr):
+    lcm_value = arr[0]
+    for num in arr[1:]:
+        lcm_value = lcm(lcm_value, num)
+    return lcm_value
