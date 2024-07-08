bd116eeb804cf75782625ceb38bb55b6e925afb2
ChanukOh <cutycater@gmail.com>
2024-07-08 10:38:40 +0900

    Time: 55 ms (15.46%), Space: 16.6 MB (10.53%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,15 +1,13 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         dic={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        answer=[]
-        for i in s:
-            answer.append(dic[i])
         count=0
         imsi=0
-        for b,x in enumerate(answer):
+        for b,y in enumerate(s):
+            x=dic[y]
             imsi=x
-            if b+1<len(answer):
-                if answer[b+1]>abs(imsi):
+            if b+1<len(s):
+                if dic[s[b+1]]>abs(imsi):
                     imsi=-x
             count+=imsi
         return count
