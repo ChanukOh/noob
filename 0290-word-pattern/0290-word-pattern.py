a2f48558c8f8ff0e1d280c8d91dbdd83cc6d0058
ChanukOh <cutycater@gmail.com>
2024-07-09 09:07:44 +0900

    Time: 38 ms (32.4%), Space: 16.4 MB (78.16%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,24 +1,19 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s,pattern=s.split(' '),list(pattern)
-        dic={}
-        answer1=[]
+        
         answer2=[]
-        a=1
-        for i in s:
-            if i in dic:
-                answer1.append(dic[i])
-            else:
-                dic[i]=a
-                a+=1
-                answer1.append(dic[i])
-        a=1
-        dic={}
-        for i in pattern:
-            if i in dic:
-                answer2.append(dic[i])
-            else:
-                dic[i]=a
-                a+=1
-                answer2.append(dic[i])
-        return answer1==answer2
+        for k in [s,pattern]:
+            dic={}
+            answer1=[]
+            a=1
+            for i in k:
+                if i in dic:
+                    answer1.append(dic[i])
+                else:
+                    dic[i]=a
+                    a+=1
+                    answer1.append(dic[i])
+            answer2.append(answer1)
+        
+        return answer2[0]==answer2[1]
